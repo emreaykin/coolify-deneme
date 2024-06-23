3e839a102110be3798df493986f6f8318e289fff
emreaykin <emre.kou.1809@gmail.com>
2024-06-23 19:42:35 +0300

    aa

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import bodyParser from "body-parser";
+
 import cors from "cors";
 
 
 dotenv.config();
 const port = 3000;
 
-app.use(bodyParser.json({ limit: "2mb" }));
+
 app.use(express.json());
 app.use(cors());
 app.listen(port, () => {
