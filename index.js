fc3f3d1f1d6376ac6ab0ef22e25d299a0be2956d
emreaykin <emre.kou.1809@gmail.com>
2024-06-23 19:50:00 +0300

    upps

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-import express from "express";
-
+import express from 'express';
 import bodyParser from "body-parser";
 import cors from "cors";
-
-
 const app = express();
 const port = 3000;
 
+// Middleware
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(express.json());
 app.use(cors());
+
+// Basit bir GET isteği
+app.get('/', (req, res) => {
+  res.send('Merhaba Dünya!');
+});
+
+// Basit bir API endpoint
+app.get('/api', (req, res) => {
+  res.json({ message: 'API çalışıyor! ' });
+});
+app.get('/apii', (req, res) => {
+    res.json({ message: 'API kanca deneme ' });
+  });
+  app.get('/web', (req, res) => {
+    res.json({ message: 'kanca' });
+  });
+// Dinleme
 app.listen(port, () => {
-  console.log(`Sunucu http://localhost:${port} bb adresinde çalışıyor`);
+  console.log(`Sunucu http://localhost:${port} aaaa adresinde çalışıyor`);
 });
